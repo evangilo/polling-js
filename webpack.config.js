ba876c0ac8b84d785750cf3a6855a070a031c233
Evangilo Morais <evangilo@evolux.net.br>
2020-06-18 13:41:34 -0300

    fixing libraryTarget

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "polling.min.js",
-    libraryTarget: "var",
+    libraryTarget: "commonjs2",
     library: "polling",
   },
   module: {
